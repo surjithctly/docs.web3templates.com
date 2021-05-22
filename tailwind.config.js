51a3475c7b0871bda538eff30753f1e16e82251d
Surjith S M <surjithctly@gmail.com>
2021-05-22 21:59:54 +0530

    remove purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,8 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-  purge: ["./pages/**/*.{md,mdx}", "./styles/*.css"],
+  //purge: ["./pages/**/*.{md,mdx}", "./styles/*.css"],
+  purge: false,
   darkMode: "class",
   theme: {
     fontFamily: {
