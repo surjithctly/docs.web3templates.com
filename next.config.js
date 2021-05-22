d18311a1f62c362dab955d3014e50b3bc78b541b
Surjith S M <surjithctly@gmail.com>
2021-05-22 22:09:43 +0530

    patch: ENOENT issue

--- next.config.js
+++ next.config.js
@@ -2,16 +2,19 @@ const withNextra = require("nextra")("nextra-theme-docs", "./theme.config.js");
 
 // module.exports = withNextra();
 
-(module.exports = withNextra()),
-  {
-    i18n: {
-      locales: ["en"],
-      defaultLocale: "en",
-    },
-    future: {
-      webpack5: true,
-    },
-  };
+module.exports = withNextra({
+  webpack(config) {
+    config.plugins.pop(); // remove stork plugin which was added by nextra. Remove when nextra version is bumped!
+    return config;
+  },
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
+  future: {
+    webpack5: true,
+  },
+});
 
 // fix enonet error
 // https://github.com/shuding/nextra/issues/136
